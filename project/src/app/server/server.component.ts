8ee56a178a2179ec85abace73da14a0c87059685
Hussain Madi <Hussain.Haitham.Madi@gmail.com>
2020-05-19 08:29:34 +0530

    Update server button text depending on isOnline state

--- project/src/app/server/server.component.ts
+++ project/src/app/server/server.component.ts
@@ -9,9 +9,28 @@ import { Server } from './../shared/server';
 export class ServerComponent implements OnInit {
 
   constructor() { }
+  color:string;
+  buttonText:string;
   @Input() serverInput:Server;
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    this.setServerStatus(this.serverInput.isOnline);
   }
 
+  setServerStatus(isOnline:boolean){
+    if(isOnline){
+      this.serverInput.isOnline =true;
+      this.color='#66886A',
+      this.buttonText = 'Shut Down';
+    }else{
+      this.serverInput.isOnline =false;
+      this.color = '#FF6868';
+      this.buttonText = 'Start';
+    }
+  }
+ toggleStatus(onLineStatus:boolean) {
+   console.log(this.serverInput.name,':',onLineStatus);
+   this.setServerStatus(!onLineStatus);
+ }
 }
+ 
